29bf43734f1c1aa0f18543b435dc63a68e3c7495
Hanyi SEO <hanyiseo@Hanyis-MacBook-Pro.local>
2022-12-27 17:10:49 +0900

    Books Create/Index/New

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 app.set('view engine','ejs')
 app.set('views',__dirname + '/views') //code check
@@ -20,9 +21,12 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { //code check
     useNewUrlParser: true})
 const db = mongoose.connection
+mongoose.set('strictQuery', false);
 db.on('error', error => console.error(error))
 db.once('open',() => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
+
 app.listen(process.env.PORT || 3000)
